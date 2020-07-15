2edf22aa4dc45461cd38464cbe17354303d3d4a0
rosebutch <annathedm@gmail.com>
2020-07-15 15:49:04 -0500

    clean out unused images, personalize 404

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import logo from '../assets/images/logo.svg';
-
-const Header = (props) => (
-    <div></div>
-)
-
-export default Header
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
+import barbie from '../assets/images/website-icon.png'
 
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist.</p>
+    <span className="image"><img src={barbie} alt="" /></span>
+
   </Layout>
 )
 
