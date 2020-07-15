240240acbcfe70e6581bb95eb74423aefd38199c
rosebutch <annathedm@gmail.com>
2020-07-15 11:58:15 -0500

    update soft skills

--- src/pages/coding.js
+++ src/pages/coding.js
@@ -4,10 +4,10 @@ import Layout from '../components/layout'
 import HeaderGeneric from '../components/HeaderGeneric'
 import codeHeaderImage from '../assets/images/code-header-image.png'
 import SkillLevel from '../components/SkillLevel'
+import {skills} from '../resume'
 
 class Coding extends React.Component {
   render() {
-    const skills = [["JavaScript", "React (hooks and lifecycle methods)", "Redux", "Node.js", "Express", "PostgreSQL", "Sequelize", "Git", "HTML", "CSS", "Firebase Auth", "Firebase Realtime Database"], ["Mocha", "Chai", "Jasmine", "Python", "Bootstrap", "React Native", "NoSQL"],["Heroku", "socket.io", "Ephemeris", "Moment", "Firestore"]]
     return (
       <Layout>
         <Helmet title="Coding Page Title" />
@@ -19,8 +19,12 @@ class Coding extends React.Component {
             <ul className="features">
               {skills.map((skillLevel, index) => <SkillLevel key={5 - index} stars={5 - index} skillArr={skillLevel} />)}
             </ul>
-            <h2>Other Skills</h2>
-            <p>I'm great at helping people feel comfortable and focus the task at hand. I'm an expert at explaining technical concepts without being overwhelmed by the details. I've also become fantastic at debugging by noticing all the details. I love to code as a team, and I live by <a href="https://www.shinetheory.com/">Aminatou Sow and Ann Friedman's Shine Theory</a> of mutual investment and collaboration. </p>
+            <h2>Soft Coding Skills</h2>
+            <ul>
+              <li>I'm great at helping people feel comfortable and focus the task at hand.</li>
+              <li>I'm an expert at explaining technical concepts without being overwhelmed by the details. I've also become fantastic at debugging by noticing all the details.</li>
+              <li>I love to code as a team, and I live by <a href="https://www.shinetheory.com/">Aminatou Sow and Ann Friedman's Shine Theory</a> of mutual investment and collaboration.</li>
+            </ul>
           </section>
         </div>
       </Layout>
